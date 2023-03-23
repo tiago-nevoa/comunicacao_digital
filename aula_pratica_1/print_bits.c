18ac859683e4dc4f7f50186db83a9b36d92f1e11
TiagoNevoa <tiagonevoa@gmail.com>
2023-03-23 17:43:43 +0000

    print_bits to int values and add gitignore

---
+++ aula_pratica_1/print_bits.c
@@ -0,0 +1,9 @@
+void print_bits( int val )
+{
+	// int in C is 32 bits we start on the most relevant bit
+	for(int i = 31; i >= 0; i--)
+	{
+        printf("%d", (val >> i) & 1);
+    }
+    printf("\n");
+}
