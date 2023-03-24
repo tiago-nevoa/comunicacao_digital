d044cff4276bcabe63e8a23bba13b580425bbe84
TiagoNevoa <tiagonevoa@gmail.com>
2023-03-24 23:08:48 +0000

    we need use the all files algoritms to do this one

---
+++ aula_pratica_1/negative_file.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_CHARS 256
+
+void negative_file( char *input_file_name, char *output_file_name){
+
+char most_frequent_symbol(char *file_name) {
+    FILE *fp;
+    int count[NUM_CHARS] = {0};
+    int max_count = 0;
+    char max_char = '\0';
+    int c;
+
+    fp = fopen(file_name, "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (c >= 0 && c < NUM_CHARS) {
+            count[c]++;
+            if (count[c] > max_count) {
+                max_count = count[c];
+                max_char = c;
+            }
+        }
+    }
+
+    fclose(fp);
+
+	/*	Change the code to read and write with the conditions:
+		a qual transforma o ficheiro de entrada input_file_name no ficheiro de saída output_file_name.
+		O ficheiro de saída é produzido a partir do ficheiro de entrada através na negação de cada bit do mesmo.
+	*/
+}
