e3259cb1f29aeb7d268fb82831e1f999ad26511f
TiagoNevoa <tiagonevoa@gmail.com>
2023-03-24 22:41:48 +0000

    most_frequent_symbol and tester

--- aula_pratica_1/test.c
+++ aula_pratica_1/test.c
@@ -3,6 +3,7 @@
 int count_ones( int val );
 void print_bits( int val );
 int count_zeros( int val );
+char most_frequent_symbol(char *file_name);
 
 int main (void)
 {
@@ -11,5 +12,13 @@ int main (void)
 	print_bits(value);
 	printf("We have %dx 1's \n",count_ones(value));
 	printf("We have %dx 0's \n",count_zeros(value));
+
+	char file_name[100];
+    char most_frequent;
+
+    most_frequent = most_frequent_symbol("ListaPalavrasEN.txt");
+
+    printf("The most frequent symbol in the file is '%c'\n", most_frequent);
+
 	return (0);
 }
