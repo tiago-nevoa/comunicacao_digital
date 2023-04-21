9e9b98375d2c278cd612bdc19cd657447e2b1f1f
TiagoNevoa <tiagonevoa@gmail.com>
2023-04-21 22:06:21 +0100

    solve git problem

--- aula_pratica_1/test.c
+++ aula_pratica_1/test.c
@@ -4,15 +4,24 @@ int count_ones( int val );
 void print_bits( int val );
 int count_zeros( int val );
 char most_frequent_symbol(char *file_name);
+void negative_file(char *input_file_name, char *output_file_name);
 
 int main (void)
 {
 	int value = 12345;
+	char my_first_file[] = "ListaPalavrasEN.txt";
+	char my_second_file[] = "ListaPalavrasPT.txt";
+	char my_first_encryption[] = "ListaPalavrasENencrypted.txt";
+	char my_first_decryption[] = "ListaPalavrasENdecrypted.txt";
 
 	print_bits(value);
 	printf("We have %dx 1's \n",count_ones(value));
 	printf("We have %dx 0's \n",count_zeros(value));
-    printf("The most frequent symbol in the file is '%c'\n", most_frequent_symbol("ListaPalavrasEN.txt"));
+    printf("The most frequent symbol in the file %s is '%c'\n", my_first_file, most_frequent_symbol(my_first_file));
+	printf("The most frequent symbol in the file %s is '%c'\n", my_second_file, most_frequent_symbol(my_second_file));
+
+	negative_file(my_first_file,my_first_encryption);
+	negative_file(my_first_encryption,my_first_decryption);
 
 	return (0);
 }
