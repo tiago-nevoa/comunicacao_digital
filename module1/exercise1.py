2402b2cbe7abaec551def6071cdc045e44adc0da
TiagoNevoa <tiagonevoa@gmail.com>
2023-05-03 23:05:16 +0100

    exc1 with histogram

--- module1/exercise1.py
+++ module1/exercise1.py
@@ -2,17 +2,18 @@
 #!/usr/bin/env python
 import math
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 files_path = Path("TestFilesCD/")
 
 # (a) Função que apresenta os primeiros N termos da progressão geométrica de primeiro termo u e razão r. Os valores de N, u e r são passados como parâmetro.
 
 def first_n_terms_gs(u, r, N):
-  terms = [u]
-  for num in range(1,N):
-    term = u * r**num
-    terms.append(term)
-  return terms
+    terms = [u]
+    for num in range(1,N):
+        term = u * r**num
+        terms.append(term)
+    return terms
 
 
 # print(first_n_terms_gs(3, 2, 5))
@@ -42,61 +43,76 @@ def mdc(a, b):
 # (c) Função que identifica os símbolos mais frequente e menos frequente de um ficheiro passado como parâmetro, indicando a frequência de ocorrência desses dois símbolos.
 
 def symb_frequencies(file):
-  frequencies = {}
-  with open(file, "r", errors='ignore') as f:
-    lines = f.readlines()
-    for line in lines:
-      symbols = list(line.strip().replace(" ", ""))
-      for char in symbols:
-        if frequencies.setdefault(char) == None:
-          frequencies[char] = 1
-        else:
-          frequencies[char] += 1
-    f.close()
-  return frequencies
-  
+    frequencies = {}
+    with open(file, "r", errors='ignore') as f:
+        lines = f.readlines()
+        for line in lines:
+            symbols = list(line.strip().replace(" ", ""))
+            for char in symbols:
+                if frequencies.setdefault(char) == None:
+                    frequencies[char] = 1
+                else:
+                    frequencies[char] += 1
+        f.close()
+    return frequencies
+
 
 def most_and_least_frequent(file):
-  frequencies = symb_frequencies(file)
+    frequencies = symb_frequencies(file)
 
-  biggest = sorted(frequencies.values())[-1]
-  smallest = sorted(frequencies.values())[0]
-  
-  most_freq = list(frequencies.keys())[list(frequencies.values()).index(biggest)]
-  least_freq = list(frequencies.keys())[list(frequencies.values()).index(smallest)]
+    biggest = sorted(frequencies.values())[-1]
+    smallest = sorted(frequencies.values())[0]
 
-  return { most_freq: biggest, least_freq: smallest }
+    most_freq = list(frequencies.keys())[list(frequencies.values()).index(biggest)]
+    least_freq = list(frequencies.keys())[list(frequencies.values()).index(smallest)]
+
+    return { most_freq: biggest, least_freq: smallest }
 
 print(most_and_least_frequent(files_path / "alice29.txt"))
 
 
 # (d) Função que apresenta o histograma de um ficheiro, o valor da informação própria de cada símbolo e a entropia do ficheiro
 
-def histogram(file): 
-  frequencies = symb_frequencies(file)
-  total_frequencies = float(sum(frequencies.values()))
+import matplotlib.pyplot as plt
+import math
 
-  self_informations = []
-  total_entropy = 0
 
-  print("________________________HISTOGRAM________________________")
-  for key in frequencies:
-    percentage = frequencies[key] / total_frequencies
-    self_information = round(math.log(1/percentage,2),2)
-    self_informations.append(self_information)
+def histogram(file):
+    frequencies = symb_frequencies(file)
+    total_frequencies = float(sum(frequencies.values()))
 
-    entropy = -(percentage * math.log(percentage,2))
-    total_entropy += entropy
-    
-    frequency = int(frequencies[key]) * '*'
+    self_informations = []
+    total_entropy = 0
 
-    print("{key}: {frequency}".format(key=key,frequency=frequency))
+    # create lists for histogram data
+    keys = []
+    counts = []
 
-  print("___________________________________________________________")
-  print(self_informations)
+    for key in frequencies:
+        percentage = frequencies[key] / total_frequencies
+        self_information = round(math.log(1 / percentage, 2), 2)
+        self_informations.append(self_information)
 
-  print("___________________________________________________________")
-  print("Total entropy: {total_entropy}".format(total_entropy=round(total_entropy,2)))
+        entropy = -(percentage * math.log(percentage, 2))
+        total_entropy += entropy
 
-  
+        frequency = int(frequencies[key])
+
+        keys.append(key)
+        counts.append(frequency)
+
+    # plot histogram
+    plt.bar(keys, counts)
+    plt.title('Histogram')
+    plt.xlabel('Symbol')
+    plt.ylabel('Count')
+    plt.show()
+
+    print("___________________________________________________________")
+    print(self_informations)
+
+    print("___________________________________________________________")
+    print("Total entropy: {total_entropy}".format(total_entropy=round(total_entropy, 2)))
+
+
 histogram(files_path / "a.txt")
