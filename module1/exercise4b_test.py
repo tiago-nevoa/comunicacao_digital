1de71e644cc227b3bc8bd72cf49bc24b110cc219
a39734 <a39734@alunos.isel.pt>
2023-05-07 19:40:57 +0100

    exercise4 done

--- module1/exercise4b_test.py
+++ module1/exercise4b_test.py
@@ -7,27 +7,32 @@ fileIn = "TestFilesCD/alice29.txt"
 fileOutEnc = "Output_Files/encrypted_file.txt"
 fileOutDec = "Output_Files/decrypted_file.txt"
 
+# key generation
 theKey = password_generator().encode('utf-8')
 print("The generator key is: ")
 print(theKey)
 
+# cypher file
 cypher_file(fileIn, fileOutEnc, theKey)
 print("File Encrypted.. Done!")
 
+# decipher_file
 cypher_file(fileOutEnc, fileOutDec, theKey)
 print("File Decrypted.. Done!")
 
-print("\nSelf Information and Entropy...\n")
+# histograms and entropies
+print("\nHistogram and Entropy...\n")
+# plain text file
 print(f'File: {fileIn}')
 histogram(fileIn)
 print("Entropy")
 print(f'H = {round(entropy(fileIn), 2)}')
-
+# cyphered file
 print(f'File: {fileOutEnc}')
 histogram(fileOutEnc)
 print("Entropy")
 print(f'H = {round(entropy(fileOutEnc), 2)}')
-
+# deciphered file
 print(f'File: {fileOutDec}')
 histogram(fileOutDec)
 print("Entropy")
