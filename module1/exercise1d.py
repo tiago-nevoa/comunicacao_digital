509f67d743a358d0819840862c238e286b5340e1
a39734 <a39734@alunos.isel.pt>
2023-05-07 18:16:39 +0100

    exercise1 done

--- module1/exercise1d.py
+++ module1/exercise1d.py
@@ -1,42 +1,43 @@
+# (d) Função que apresenta
+# o histograma de um ficheiro,
+# o valor da informação própria de cada símbolo
+# e a entropia do ficheiro
+import math
+from matplotlib import pyplot as plt
 from module1.exercise1c import *
 
-# (d) Função que apresenta o histograma de um ficheiro, o valor da informação própria de cada símbolo e a entropia do ficheiro
 def histogram(file):
+    frequencies = symb_frequencies(file)
+    plt.bar(frequencies.keys(), frequencies.values())
+    plt.title(f'{file}')
+    plt.xlabel('Symbol')
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+def self_informations(file):
     frequencies = symb_frequencies(file)
     total_frequencies = float(sum(frequencies.values()))
 
-    self_informations = []
-    total_entropy = 0
-
-    # create lists for histogram data
-    keys = []
-    counts = []
+    self_informations = {}
 
     for key in frequencies:
         percentage = frequencies[key] / total_frequencies
         self_information = round(math.log(1 / percentage, 2), 2)
-        self_informations.append(self_information)
+        self_informations[key] = self_information
 
+    return self_informations
+
+def entropy(file):
+    total_entropy = 0
+
+    frequencies = symb_frequencies(file)
+    total_frequencies = float(sum(frequencies.values()))
+
+    for key in frequencies:
+        percentage = frequencies[key] / total_frequencies
         entropy = -(percentage * math.log(percentage, 2))
         total_entropy += entropy
 
-        frequency = int(frequencies[key])
+    return total_entropy
 
-        keys.append(key)
-        counts.append(frequency)
-
-    # plot histogram
-    plt.bar(keys, counts)
-    plt.title(f'{file}')
-    plt.xlabel('Symbol')
-    plt.ylabel('Count')
-    plt.show()
-
-    print("___________________________________________________________")
-    print(self_informations)
-
-    print("___________________________________________________________")
-    print("Total entropy: {total_entropy}".format(total_entropy=round(total_entropy, 2)))
-
-
-histogram("TestFilesCD/a.txt")
