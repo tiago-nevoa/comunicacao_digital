48a25b2e88cf259b0e65fd93eb56fd712036f5c9
Margarida Toureiro <margaridatoureiro@gmail.com>
2023-05-08 16:09:11 +0100

    exerce 5 b) i) finished - still gotta confirm but looks okay; 5b) ii) in progress

---
+++ module1/exercise5b_ii.py
@@ -0,0 +1,11 @@
+def interleaving_bsc_system(binary_sequence, p, matrix_dim):
+    input_data = binary_symmetric_channel(binary_sequence, p)
+
+    interleaving(input_data, matrix_dim)
+
+
+
+
+
+
+
