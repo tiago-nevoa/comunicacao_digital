93cc46b8486d512d2c881abf64b2aa7d7104e2ec
Margarida Toureiro <margaridatoureiro@gmail.com>
2023-06-07 18:15:40 +0100

    exercise-1a-ii finished; to fix: some files are not being read properly in the tests

---
+++ module2/exercise1b.py
@@ -0,0 +1,3 @@
+# (b) Repita a alínea anterior, considerando agora a aplicação a técnica de entrelaçamento (interleaving), tal como esquematizado na Figura 2. Para a transmissão de cada ficheiro e para todos os valores de p: indique o número de símbolos
+# diferentes entre os ficheiros A e B; indique o número de símbolos diferentes entre os ficheiros Tx e Rx Comente os
+# resultados.
