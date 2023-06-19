fab44feef2af087dd126604754cd46220350fe52
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2023-06-19 15:22:37 +0100

    port_reader : mac_connectuion

--- module2/raspberrypi-code/port_reader.py
+++ module2/raspberrypi-code/port_reader.py
@@ -1,7 +1,7 @@
 import serial
 
 # Open the serial port
-ser = serial.Serial('/dev/ttyUSB0', 9600)  # Replace with the appropriate baud rate
+ser = serial.Serial('/dev/tty.usbserial-1433240', 9600)  # Replace with the appropriate baud rate
 
 # Read data from the serial port
 while 1:
