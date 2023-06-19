38184872f19143f0f8093432802e97df5510c39c
micaela-fm <91810255+micaela-fm@users.noreply.github.com>
2023-06-19 19:55:14 +0100

    exercise1aiii.py: gave up on it xD

--- module2/exercise1aiii.py
+++ module2/exercise1aiii.py
@@ -8,73 +8,82 @@ sys.path.append(parent_dir)
 from module1.exercise5a import binary_symmetric_channel
 from module1.exercise4a import stringToBinary
 
-def hamming_code_bsc_correction_mode(file, error_rate):
+
+# (ii) BER3, após a aplicação de código de Hamming (7, 4) sobre o BSC, em modo de correção;
+
+def BER_hamming_code_bsc_correction_mode(file, error_rate):
     with open(file, 'r') as input_file:
         contents = input_file.read()
         input_bits = bin(stringToBinary(contents)).replace("0b", "")
 
+    # ensure the input_bits are divisible by 4
     for i in range((4 - len(input_bits) % 4) % 4):
         input_bits = "0" + input_bits
 
     encoded_bits = []
 
     for i in range(0, len(input_bits), 4):
+
         message = input_bits[i:i + 4]
 
         # Calculate parity bits
-        parity_0 = str((int(message[0]) + int(message[1]) + int(message[3])) % 2)
-        parity_1 = str((int(message[0]) + int(message[2]) + int(message[3])) % 2)
-        parity_2 = str((int(message[1]) + int(message[2]) + int(message[3])) % 2)
+        parity_0 = str(int(message[1]) ^ int(message[2]) ^ int(message[3]))
+        parity_1 = str(int(message[0]) ^ int(message[1]) ^ int(message[3]))
+        parity_2 = str(int(message[0]) ^ int(message[2]) ^ int(message[3]))
 
         # Construct the encoded bits
         encoded_bits += [message, parity_0, parity_1, parity_2]
 
     output_bits = binary_symmetric_channel("".join(encoded_bits), error_rate)
 
+    decoded_bits = ''
     error_positions = []
-    transpose_parity_matrix = [['1', '1', '0'],
-                               ['1', '0', '1'],
-                               ['0', '1', '1'],
-                               ['1', '1', '1'],
-                               ['1', '0', '0'],
-                               ['0', '1', '0'],
-                               ['0', '0', '1']]
+    transpose_parity_matrix = [[1, 0, 0],
+                               [1, 0, 1],
+                               [0, 1, 1],
+                               [1, 1, 1],
+                               [1, 0, 0],
+                               [0, 1, 0],
+                               [0, 0, 1]]
 
     # Decode the received bits
-    corrected_output_bits = ''
     for i in range(0, len(output_bits), 7):
+        # Check if there are enough bits remaining to form a complete group
+        if i + 7 > len(output_bits):
+            break
+
         codeword = output_bits[i:i + 7]
 
+
         # Calculate syndrome bits
-        syndrome_0 = str((int(codeword[0]) + int(codeword[1]) + int(codeword[3]) + int(codeword[4])) % 2)
-        syndrome_1 = str((int(codeword[0]) + int(codeword[2]) + int(codeword[3]) + int(codeword[5])) % 2)
-        syndrome_2 = str((int(codeword[1]) + int(codeword[2]) + int(codeword[3]) + int(codeword[6])) % 2)
+        syndrome_0 = str(int(codeword[0]) ^ int(codeword[1]) ^ int(codeword[3]) ^ int(codeword[4]))
+        syndrome_1 = str(int(codeword[0]) ^ int(codeword[2]) ^ int(codeword[3]) ^ int(codeword[5]))
+        syndrome_2 = str(int(codeword[1]) ^ int(codeword[2]) ^ int(codeword[3]) ^ int(codeword[6]))
         syndrome = [syndrome_2, syndrome_1, syndrome_0]
 
         # Check if any error occurred
-        total_errors = 0
+        if int(syndrome_2 + syndrome_1 + syndrome_0, 2) != 0:
+            # Error detected, find the position of the error
+            for index, line in enumerate(transpose_parity_matrix):
+                    if syndrome == line:
+                        error_positions += index
 
-        if int(syndrome_2 + syndrome_1 + syndrome_0, 2) == 0:
-            corrected_output_bits += codeword
-        else:  # Error detected, find the position of the error
-            if syndrome in transpose_parity_matrix:
-                error_positions.append(transpose_parity_matrix.index(syndrome))
-                total_errors += 1
+            # error_position = int(syndrome_2 + syndrome_1 + syndrome_0, 2) - 1
+            # error_positions.append(i + error_position)
 
-            # Correct the errors
-            for position in error_positions:
-                corrected_bit = '1' if codeword[position] == '0' else '0'
-                codeword = codeword[:position] + corrected_bit + codeword[position + 1:]
+        decoded_bits += codeword[0] + codeword[1] + codeword[2] + codeword[4]  # Ignore parity bits
 
-            corrected_output_bits += codeword
+    # Correct the errors
+    for position in error_positions:
+        corrected_bit = '1' if output_bits[position] == '0' else '0'
+        output_bits = output_bits[:position] + corrected_bit + output_bits[position + 1:]
 
     # Calculate Bit Error Rate (BER)
-    total_errors = sum([int(a) != int(b) for a, b in zip("".join(encoded_bits), corrected_output_bits)])
+    total_errors = sum([int(a) != int(b) for a, b in zip("".join(encoded_bits), output_bits)])
     total_bits = len("".join(encoded_bits))
     BER = round(total_errors / total_bits, 20)
 
     return BER
-
 
 def count_bits_through_BSC(file):
     with open(file, 'rb') as input_file:
--- module2/exercise1aiii_test.py
+++ module2/exercise1aiii_test.py
@@ -1,38 +1,41 @@
-
-from exercise1aiii import hamming_code_bsc_correction_mode, count_bits_through_BSC
+from exercise1aiii import BER_hamming_code_bsc_correction_mode, count_bits_through_BSC
 from module2.exercise1ai import count_different_symbols
 
-error_rates = [pow(10,-1), pow(10,-2), pow(10,-3), pow(10,-4), pow(10,-5)]
-list_of_files = ["TestFilesCD/a.txt", "TestFilesCD/alice29.txt", "TestFilesCD/cp.htm", "TestFilesCD/Person.java","TestFilesCD/progc.c"]
+error_rates = [pow(10, -1), pow(10, -2), pow(10, -3), pow(10, -4), pow(10, -5)]
+list_of_files = ["TestFilesCD/a.txt",
+                 "TestFilesCD/alice29.txt",
+                 "TestFilesCD/cp.htm",
+                 "TestFilesCD/Person.java",
+                 "TestFilesCD/progc.c"]
 
-# for file in list_of_files:
-#     print("-------------------------")
-#     print(file)
-#     print("-------------------------")
-#     bits_through_BSC = count_bits_through_BSC(file)
-#     print("Number of bits through the BSC:", bits_through_BSC)
-#     print("For each p in", error_rates)
-#     print()
-#     for error_rate in error_rates:
-#         BER = BER_hamming_code_bsc_correction_mode(file, error_rate)
-#         different_symbols = count_different_symbols(file, error_rate)
-#         print("p:", error_rate)
-#         print("BER:", BER)
-#         print("Number of different symbols:", different_symbols)
-#         print()
-
-file = list_of_files[4]
-print("-------------------------")
-print(file)
-print("-------------------------")
-bits_through_BSC = count_bits_through_BSC(file)
-print("Number of bits through the BSC:", bits_through_BSC)
-print("For each p in", error_rates)
-print()
-for error_rate in error_rates:
-    BER = hamming_code_bsc_correction_mode(file, error_rate)
-    different_symbols = count_different_symbols(file, error_rate)
-    print("p:", error_rate)
-    print("BER:", BER)
-    print("Number of different symbols:", different_symbols)
+for file in list_of_files:
+    print("-------------------------")
+    print(file)
+    print("-------------------------")
+    bits_through_BSC = count_bits_through_BSC(file)
+    print("Number of bits through the BSC:", bits_through_BSC)
+    print("For each p in", error_rates)
     print()
+    for error_rate in error_rates:
+        BER = BER_hamming_code_bsc_correction_mode(file, error_rate)
+        different_symbols = count_different_symbols(file, error_rate)
+        print("p:", error_rate)
+        print("BER:", BER)
+        print("Number of different symbols:", different_symbols)
+        print()
+
+# file = list_of_files[2]
+# print("-------------------------")
+# print(file)
+# print("-------------------------")
+# bits_through_BSC = count_bits_through_BSC(file)
+# print("Number of bits through the BSC:", bits_through_BSC)
+# print("For each p in", error_rates)
+# print()
+# for error_rate in error_rates:
+#     BER = hamming_code_bsc_correction_mode(file, error_rate)
+#     different_symbols = count_different_symbols(file, error_rate)
+#     print("p:", error_rate)
+#     print("BER:", BER)
+#     print("Number of different symbols:", different_symbols)
+#     print()
