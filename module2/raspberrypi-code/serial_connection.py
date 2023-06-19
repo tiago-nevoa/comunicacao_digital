34813f3f5bdcfdf0bae5880591e7db8ac4cc1716
Margarida Toureiro <margaridatoureiro@gmail.com>
2023-06-19 15:20:05 +0100

    raspberry pi

--- module2/raspberrypi-code/serial_connection.py
+++ module2/raspberrypi-code/serial_connection.py
@@ -2,11 +2,12 @@ import serial
 import os 
 
 # Configure the serial connection
-ser = serial.Serial('/dev/ttyS0', 9600)  # Replace '/dev/ttyUSB0' with the appropriate device file and baud rate
-while 1:
-# Write data to the serial port
-    data = "Hello, World, Serial Receiver" + os.linesep
-    ser.write(data.encode())
+def write_to_port(input_data):
+    ser = serial.Serial('/dev/ttyS0', 9600)  
+    while 1:
+    # Write data to the serial port
+        data = input_data + os.linesep
+        ser.write(data.encode())
 
-# Close the serial connection
-ser.close()        
+    # Close the serial connection
+    ser.close()        
