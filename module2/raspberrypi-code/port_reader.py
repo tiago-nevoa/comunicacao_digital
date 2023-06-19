b5e3b4b5122b73ab65d414f72854093699fee0c0
Margarida Toureiro <margaridatoureiro@gmail.com>
2023-06-19 17:49:21 +0100

    fixes

--- module2/raspberrypi-code/port_reader.py
+++ module2/raspberrypi-code/port_reader.py
@@ -1,15 +1,21 @@
 import serial
+import ast
 from fletcher_checksum import *
 
 # Open the serial port
-ser = serial.Serial('/dev/tty.usbserial-1433240', 9600)  # Replace with the appropriate baud rate
+ser = serial.Serial('/dev/tty.usbserial-1433240', 9600)
 
 # Read data from the serial port
 while 1:
     data = ser.readline()
-    print(data.decode())  # Convert bytes to string and print
-    receive_fletcher_checksum = ser.readline()
-    print("fletcher checksum : " + receive_fletcher_checksum.decode())
+    print(data.decode())
+
+    received_fletcher_checksum = ser.readline()
+    print("fletcher checksum : " + received_fletcher_checksum.decode())
+
+    data_array = ast.literal_eval(data.decode())
+    ("fletcher checksum validation: " + verify_fletcher_checksum(data_array, int(received_fletcher_checksum)))
+
 
 # Close the serial port
 ser.close()
