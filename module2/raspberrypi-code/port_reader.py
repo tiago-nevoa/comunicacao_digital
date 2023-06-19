9bc2d89d80e02ff04dfb8be7ef2b7abc4267c14f
Tiago M. F. Nevoa <tiagonevoa@gmail.com>
2023-06-19 19:56:56 +0100

    Update port_reader.py
    error detection flag
    
    Co-Authored-By: Margarida Toureiro <56609720+margaridatoureiro@users.noreply.github.com>
    Co-Authored-By: Micaela FM <91810255+micaela-fm@users.noreply.github.com>

--- module2/raspberrypi-code/port_reader.py
+++ module2/raspberrypi-code/port_reader.py
@@ -7,20 +7,20 @@ ser = serial.Serial('/dev/tty.usbserial-1433240', 9600)
 
 while 1:
     # read data from the serial port
-    data = ser.readline()
-    print(data.decode())
+    data = ser.readline().decode()
+    print(data)
 
     # read error verification option from the serial port
-    error_detection = ser.readline()
-    print(error_detection.decode())
+    error_detection = ser.readline().decode().strip()
+    print("Error Detection : " + error_detection)
 
-    if error_detection == 'True':
+    if error_detection == "True":
         # read original checksum result from the serial port
-        original_checksum = int(ser.readline())
-        print("fletcher checksum : " + original_checksum.decode())
+        original_checksum = int(ser.readline().decode())
+        print("fletcher checksum : ", original_checksum)
 
         # verify if the checksum of the original data matches the one of the transmitted data
-        data_array = ast.literal_eval(data.decode())
+        data_array = ast.literal_eval(data)
         print(data_array)
         print("fletcher checksum validation: ", verify_fletcher_checksum(data_array, original_checksum))
 
