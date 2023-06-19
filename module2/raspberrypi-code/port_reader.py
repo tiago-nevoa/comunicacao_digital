dabcb5f02d857166053d809903166d01831496ef
Margarida Toureiro <margaridatoureiro@gmail.com>
2023-06-19 17:32:19 +0100

    updated read / write to support fletcher checksum computation

--- module2/raspberrypi-code/port_reader.py
+++ module2/raspberrypi-code/port_reader.py
@@ -1,5 +1,5 @@
 import serial
-import fletcher_checksum
+from fletcher_checksum import *
 
 # Open the serial port
 ser = serial.Serial('/dev/tty.usbserial-1433240', 9600)  # Replace with the appropriate baud rate
@@ -10,9 +10,6 @@ while 1:
     print(data.decode())  # Convert bytes to string and print
     receive_fletcher_checksum = ser.readline()
     print("fletcher checksum : " + receive_fletcher_checksum.decode())
-    confirm_fletcher_checksum = fletcher_checksum.verify_fletcher_checksum(data.decode(),
-                                                                           receive_fletcher_checksum.decode())
-    print("confirm fletcher checksum : " + confirm_fletcher_checksum)
 
 # Close the serial port
 ser.close()
